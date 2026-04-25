9e4e76bf663fef269cfb6b3aa9190416b5fb8822
maxim283b <borisov.ma@list.ru>
2026-04-25 12:13:16 +0300

    Shorten migration revision id

--- alembic/versions/0002_add_agent_run_chat_telegram_id.py
+++ alembic/versions/0002_add_agent_run_chat_telegram_id.py
@@ -1,6 +1,6 @@
 """Add telegram chat id to agent runs
 
-Revision ID: 0002_add_agent_run_chat_telegram_id
+Revision ID: 0002_chat_telegram_id
 Revises: 0001_initial
 Create Date: 2026-04-25 00:00:00.000000
 """
@@ -10,7 +10,7 @@ from __future__ import annotations
 from alembic import op
 import sqlalchemy as sa
 
-revision = "0002_add_agent_run_chat_telegram_id"
+revision = "0002_chat_telegram_id"
 down_revision = "0001_initial"
 branch_labels = None
 depends_on = None
